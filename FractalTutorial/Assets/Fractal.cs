22929119b602076222bfa05302de1b5be10aed70
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2015-09-23 09:01:48 +0200

    Added Fractal and Noise tutorial projects
    Added two tutorial projects developed prior to the main project.
    Changed .gitignore to function properly with subdirectories.

---
+++ FractalTutorial/Assets/Fractal.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+	public float childScale;
+
+	private int depth;
+
+	private static Vector3[] childDirections = {
+		Vector3.up,
+		Vector3.right,
+		Vector3.left,
+		Vector3.forward,
+		Vector3.back
+	};
+
+	private static Quaternion[] childOrientations = {
+		Quaternion.identity,
+		Quaternion.Euler(0f, 0f, -90f),
+		Quaternion.Euler(0f, 0f, 90f),
+		Quaternion.Euler(90f, 0f, 0f),
+		Quaternion.Euler(-90f, 0f, 0f)
+	};
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer> ().material = material;
+		GetComponent<MeshRenderer> ().material.color = Color.Lerp (Color.gray, Color.white, (float)depth / maxDepth);
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+	
+	private void Initialize (Fractal parent, int childIndex) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+		transform.localRotation = childOrientations[childIndex];
+	}
+
+	private IEnumerator CreateChildren() {
+		for (int i = 0; i < childDirections.Length; i++) {
+			yield return new WaitForSeconds(Random.Range (0.1f, 0.5f));
+		
+			new GameObject ("Fractal Child").AddComponent<Fractal> ().Initialize (this, i);
+		}
+	}
+}
---
+++ NoiseTutorial/Assets/TextureCreator.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureCreator : MonoBehaviour {
+
+	[Range(2, 1024)]
+	public int resolution = 256;
+
+	public float frequency = 8f;
+
+	[Range(1, 3)]
+	public int dimensions = 3;
+
+	public NoiseMethodType type;
+
+	public bool useFractalNoise = false;
+
+	[Range(2, 8)]
+	public int octaves = 2;
+
+	[Range(1f, 4f)]
+	public float lacunarity = 2f;
+
+	[Range(0f, 1f)]
+	public float persistence = 0.5f;
+
+	private Texture2D texture;
+
+	private void OnEnable() {
+		if (texture == null) {
+			texture = new Texture2D (resolution, resolution, TextureFormat.RGB24, true);
+			texture.name = "Procedural Texture";
+			texture.wrapMode = TextureWrapMode.Clamp;
+			texture.filterMode = FilterMode.Trilinear;
+			texture.anisoLevel = 9;
+			GetComponent<MeshRenderer> ().material.mainTexture = texture;
+		}
+
+		FillTexture ();
+	}
+
+	public void FillTexture() {
+		if (texture.width != resolution) {
+			texture.Resize(resolution, resolution);
+		}
+
+		Vector3 point00 = transform.TransformPoint(new Vector3 (-0.5f, -0.5f));
+		Vector3 point10 = transform.TransformPoint(new Vector3 ( 0.5f, -0.5f));
+		Vector3 point01 = transform.TransformPoint(new Vector3 (-0.5f,  0.5f));
+		Vector3 point11 = transform.TransformPoint(new Vector3 ( 0.5f,  0.5f));
+
+		NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
+		float stepSize = 1f / resolution;
+		for (int y = 0; y < resolution; y++) {
+			Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
+			Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
+			for (int x = 0; x < resolution; x++) {
+				Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
+				float sample = useFractalNoise ? 
+					Noise.Sum(method, point, frequency, octaves, lacunarity, persistence) :
+					method(point, frequency);
+				if (type != NoiseMethodType.Value) {
+					sample = sample * 0.5f + 0.5f;
+				}
+				texture.SetPixel(x, y, Color.white * sample);
+			}
+		}
+		texture.Apply ();
+	}
+
+	private void Update() {
+		if (transform.hasChanged) {
+			transform.hasChanged = false;
+			FillTexture();
+		}
+	}
+}
